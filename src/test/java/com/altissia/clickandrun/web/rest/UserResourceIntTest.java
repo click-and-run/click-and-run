a22028b9fc9ea7a8865c9e4bb4f012eafd544de7
Adrien Horgnies <aHorgnies@altissia.com>
2019-04-28 18:06:21 +0200

    generate entity user
    a step towards #9

---
+++ src/test/java/com/altissia/clickandrun/web/rest/UserResourceIntTest.java
@@ -0,0 +1,314 @@
+package com.altissia.clickandrun.web.rest;
+
+import com.altissia.clickandrun.ClickAndRunApp;
+import com.altissia.clickandrun.domain.User;
+import com.altissia.clickandrun.repository.UserRepository;
+import com.altissia.clickandrun.web.rest.errors.ExceptionTranslator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see UserResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ClickAndRunApp.class)
+public class UserResourceIntTest {
+
+    private static final String DEFAULT_FIRST_NAME = "Adrien";
+    private static final String UPDATED_FIRST_NAME = "Renaud";
+
+    private static final String DEFAULT_LAST_NAME = "Horgnies";
+    private static final String UPDATED_LAST_NAME = "Laloux";
+
+    private static final String DEFAULT_LOGIN = "ahorgnies@altissia.org";
+    private static final String UPDATED_LOGIN = "rlaloux@altissia.org";
+
+    private static final Locale DEFAULT_INTERFACE_LANGUAGE = Locale.FRANCE;
+    private static final Locale UPDATED_INTERFACE_LANGUAGE = Locale.UK;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    private MockMvc restUserMockMvc;
+
+    private User user;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        UserResource userResource = new UserResource(userRepository);
+        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static User createEntity(EntityManager em) {
+        return new User()
+            .firstName(DEFAULT_FIRST_NAME)
+            .lastName(DEFAULT_LAST_NAME)
+            .login(DEFAULT_LOGIN)
+            .interfaceLanguage(DEFAULT_INTERFACE_LANGUAGE);
+    }
+
+    @Before
+    public void initTest() {
+        user = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createUser() throws Exception {
+        int databaseSizeBeforeCreate = userRepository.findAll().size();
+
+        // Create the User
+        restUserMockMvc.perform(post("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isCreated());
+
+        // Validate the User in the database
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeCreate + 1);
+        User testUser = userList.get(userList.size() - 1);
+        assertThat(testUser.getFirstName()).isEqualTo(DEFAULT_FIRST_NAME);
+        assertThat(testUser.getLastName()).isEqualTo(DEFAULT_LAST_NAME);
+        assertThat(testUser.getLogin()).isEqualTo(DEFAULT_LOGIN);
+        assertThat(testUser.getInterfaceLanguage()).isEqualTo(DEFAULT_INTERFACE_LANGUAGE);
+    }
+
+    @Test
+    @Transactional
+    public void createUserWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = userRepository.findAll().size();
+
+        // Create the User with an existing ID
+        user.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restUserMockMvc.perform(post("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Alice in the database
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void checkLastNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = userRepository.findAll().size();
+        // set the field null
+        user.setLastName(null);
+
+        // Create the User, which fails.
+
+        restUserMockMvc.perform(post("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isBadRequest());
+
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkLoginIsRequired() throws Exception {
+        int databaseSizeBeforeTest = userRepository.findAll().size();
+        // set the field null
+        user.setLogin(null);
+
+        // Create the User, which fails.
+
+        restUserMockMvc.perform(post("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isBadRequest());
+
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkInterfaceLanguageIsRequired() throws Exception {
+        int databaseSizeBeforeTest = userRepository.findAll().size();
+        // set the field null
+        user.setInterfaceLanguage(null);
+
+        // Create the User, which fails.
+
+        restUserMockMvc.perform(post("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isBadRequest());
+
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllUsers() throws Exception {
+        // Initialize the database
+        userRepository.saveAndFlush(user);
+
+        // Get all the userList
+        restUserMockMvc.perform(get("/api/users?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(user.getId().intValue())))
+            .andExpect(jsonPath("$.[*].firstName").value(hasItem(DEFAULT_FIRST_NAME)))
+            .andExpect(jsonPath("$.[*].lastName").value(hasItem(DEFAULT_LAST_NAME)))
+            .andExpect(jsonPath("$.[*].login").value(hasItem(DEFAULT_LOGIN)))
+            .andExpect(jsonPath("$.[*].interfaceLanguage").value(hasItem(DEFAULT_INTERFACE_LANGUAGE.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getUser() throws Exception {
+        // Initialize the database
+        userRepository.saveAndFlush(user);
+
+        // Get the user
+        restUserMockMvc.perform(get("/api/users/{id}", user.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(user.getId().intValue()))
+            .andExpect(jsonPath("$.firstName").value(DEFAULT_FIRST_NAME))
+            .andExpect(jsonPath("$.lastName").value(DEFAULT_LAST_NAME))
+            .andExpect(jsonPath("$.login").value(DEFAULT_LOGIN))
+            .andExpect(jsonPath("$.interfaceLanguage").value(DEFAULT_INTERFACE_LANGUAGE.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingUser() throws Exception {
+        // Get the user
+        restUserMockMvc.perform(get("/api/users/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateUser() throws Exception {
+        // Initialize the database
+        userRepository.saveAndFlush(user);
+        int databaseSizeBeforeUpdate = userRepository.findAll().size();
+
+        // Update the user
+        User updatedUser = userRepository.findOne(user.getId());
+        updatedUser
+            .firstName(UPDATED_FIRST_NAME)
+            .lastName(UPDATED_LAST_NAME)
+            .login(UPDATED_LOGIN)
+            .interfaceLanguage(UPDATED_INTERFACE_LANGUAGE);
+
+        restUserMockMvc.perform(put("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedUser)))
+            .andExpect(status().isOk());
+
+        // Validate the User in the database
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeUpdate);
+        User testUser = userList.get(userList.size() - 1);
+        assertThat(testUser.getFirstName()).isEqualTo(UPDATED_FIRST_NAME);
+        assertThat(testUser.getLastName()).isEqualTo(UPDATED_LAST_NAME);
+        assertThat(testUser.getLogin()).isEqualTo(UPDATED_LOGIN);
+        assertThat(testUser.getInterfaceLanguage()).isEqualTo(UPDATED_INTERFACE_LANGUAGE);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingUser() throws Exception {
+        int databaseSizeBeforeUpdate = userRepository.findAll().size();
+
+        // Create the User
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restUserMockMvc.perform(put("/api/users")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(user)))
+            .andExpect(status().isCreated());
+
+        // Validate the User in the database
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeUpdate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void deleteUser() throws Exception {
+        // Initialize the database
+        userRepository.saveAndFlush(user);
+        int databaseSizeBeforeDelete = userRepository.findAll().size();
+
+        // Get the user
+        restUserMockMvc.perform(delete("/api/users/{id}", user.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(User.class);
+        User user1 = new User();
+        user1.setId(1L);
+        User user2 = new User();
+        user2.setId(user1.getId());
+        assertThat(user1).isEqualTo(user2);
+        user2.setId(2L);
+        assertThat(user1).isNotEqualTo(user2);
+        user1.setId(null);
+        assertThat(user1).isNotEqualTo(user2);
+    }
+}
