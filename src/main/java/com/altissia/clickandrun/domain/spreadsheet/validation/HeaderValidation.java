bb76fd59bb1256bc6a2b0530c617926ffb0ce06c
Adrien Horgnies <aHorgnies@altissia.com>
2019-05-15 16:55:07 +0200

    add column getter for jackson

--- src/main/java/com/altissia/clickandrun/domain/spreadsheet/validation/HeaderValidation.java
+++ src/main/java/com/altissia/clickandrun/domain/spreadsheet/validation/HeaderValidation.java
@@ -17,6 +17,10 @@ public class HeaderValidation extends FieldValidation {
         this.column = column;
     }
 
+    public int getColumn() {
+        return column;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
