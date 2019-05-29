48345d1dabaf3363deff3f491faca0e310317ac2
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-05-29 14:20:42 +0200

    spelling

--- src/main/java/com/altissia/clickandrun/service/extended/validator/WorkbookValidator.java
+++ src/main/java/com/altissia/clickandrun/service/extended/validator/WorkbookValidator.java
@@ -6,16 +6,16 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * Interface to define a post-validator for business / functional validation in the hole workbook
+ * Interface to define a post-validator for business / functional validation in the whole workbook
  */
 public abstract class WorkbookValidator {
 
 	protected static final Logger log = LoggerFactory.getLogger(WorkbookValidator.class);
 	
 	/**
-	 * Assert if this validator is suitable for a particular sheet
+	 * Assert if this validator is suitable for a particular workbook
 	 * @param definition the definition to check
-	 * @return true if this validator is suitable for the provided sheet
+	 * @return true if this validator is suitable for the provided workbook
 	 */
 	public abstract boolean isApplicableTo(Workbook definition);
 	
